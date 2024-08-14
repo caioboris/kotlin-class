13ff85b47d3e81c90f27cff4bd658fb2f2bc666f
caioboris <caiogamer2000@gmail.com>
2024-08-13 22:54:19 -0300

    Creating resource calculator layout

---
+++ Calculadora_Soma/app/src/main/java/br/com/fiap/calculadorasoma/CalculadoraActivity.kt
@@ -0,0 +1,13 @@
+package br.com.fiap.calculadorasoma
+
+import android.app.Activity
+import android.os.Bundle
+
+class CalculadoraActivity : Activity()
+{
+    override fun onCreate(bundle: Bundle?){
+        super.onCreate(bundle);
+
+        setContentView(R.layout.calculadora_layout);
+    }
+}
